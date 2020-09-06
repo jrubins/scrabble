e3de7b14d58e45eb37d3b5685e84b606e4288917
Jonathan Rubins <jonrubins@gmail.com>
2020-09-06 10:16:57 -0500

    Don't open XState inspector in production.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { inspect } from '@xstate/inspect'
+import { isDevelopment } from './utils/environment'
 import { setConfig } from 'react-hot-loader'
 
 // Import the entry SCSS file for styles.
@@ -9,7 +10,9 @@ import './styles/main.scss'
 
 import App from './components/App'
 
-inspect({ iframe: false })
+if (isDevelopment()) {
+  inspect({ iframe: false })
+}
 
 setConfig({
   reloadHooks: false,
